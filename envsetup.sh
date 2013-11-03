accb0fe9b12e01e1b1c76870e2d253c40bc74547
Emilio López <turl@tuxfamily.org>
2013-11-03 13:04:27 -0300

    envsetup: drop unnecesary exports
    export -f is a bashism and we don't actually need it, so lets remove it.
    
    Change-Id: I9228cef0c4a9543b3d34fdc39a368ad0238431e2
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1648,7 +1648,6 @@ function aospremote()
     git remote add aosp https://android.googlesource.com/$PFX$PROJECT
     echo "Remote 'aosp' created"
 }
-export -f aospremote
 
 function cafremote()
 {
@@ -1665,8 +1664,6 @@ function cafremote()
     git remote add caf git://codeaurora.org/$PFX$PROJECT
     echo "Remote 'caf' created"
 }
-export -f cafremote
-
 
 function installboot()
 {
